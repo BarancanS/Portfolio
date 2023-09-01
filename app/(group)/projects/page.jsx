4c2806821d3507123e0b975b0a9b82db641c29db
BarancanS <barancansng@gmail.com>
2023-09-01 21:47:43 +0300

    Update

--- app/(group)/projects/page.jsx
+++ app/(group)/projects/page.jsx
@@ -123,6 +123,25 @@ export default function Projects() {
             </Link>
           </button>
         </div>
+        <div
+          style={{ backgroundImage: "url(UmutCan.png)" }}
+          className="flex flex-row items-center justify-center bg-cover bg-no-repeat bg-center max-sm:w-60 max-sm:h-40 w-80 h-60 border-2 border-indigo-900  rounded-md hover:scale-110 transition duration-400 ease-in-out"
+        >
+          <button className="text-orange-700 opacity-0 hover:opacity-100 w-full h-full">
+            <Link
+              href="/"
+              className="py-3 px-3 border-2 border-orange-400 rounded-full"
+            >
+              Live
+            </Link>
+            <Link
+              href="/"
+              className="py-3 px-2 border-2 border-orange-400 rounded-full ml-4"
+            >
+              Code
+            </Link>
+          </button>
+        </div>
       </section>
     </main>
   );
