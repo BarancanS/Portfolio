19dc56311b8e1282433723f8ee8bbe3057b98967
BarancanS <barancansng@gmail.com>
2023-09-15 19:35:13 +0300

    Update Button.jsx

--- app/components/Button/Button.jsx
+++ app/components/Button/Button.jsx
@@ -5,7 +5,7 @@ const Button = () => {
     <div>
       <Link
         target="_blank"
-        href="https://drive.google.com/file/d/1_LHWp8IPYpeDUy4oDGXVrugpEm-4G3Jb/view"
+        href="https://drive.google.com/file/d/1_LHWp8IPYpeDUy4oDGXVrugpEm-4G3Jb/view?usp=sharing"
       >
         <button className="transition duration-400 ease-in-out hover:scale-105 mt-10 bg-indigo-900 py-3 px-5 rounded-3xl text-xl max-sm:py-1">
           Detail
