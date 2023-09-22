e74999e9b33b9d38fb8151e4985bd523a87ff7fb
BarancanS <barancansng@gmail.com>
2023-09-22 21:59:07 +0300

    Update

--- app/(group)/projects/page.jsx
+++ app/(group)/projects/page.jsx
@@ -10,19 +10,19 @@ export default function Projects() {
       <h1 className="text-7xl mb-10 max-md:text-4xl text-center">Projects</h1>
       <section className="display grid grid-cols-3 max-md:grid-cols-1 max-lg:grid-cols-2 gap-7">
         <div
-          style={{ backgroundImage: "url(Baranflix.png)" }}
+          style={{ backgroundImage: "url(moviessdb.png)" }}
           className="flex flex-row items-center justify-center bg-cover bg-no-repeat bg-center max-sm:w-60 max-sm:h-40 w-80 h-60 rounded-md hover:scale-110 transition duration-500 ease-in-out"
         >
           <div className="flex flex-row justify-center items-center gap-2 bg-black/[.63] text-black font-semibold opacity-0 hover:opacity-100 w-full h-full transition duration-500 ease-in-out">
             <Link
-              href="https://baranflix.vercel.app/"
+              href="https://moviessdb.vercel.app/"
               target="_blank"
               className="w-20 h-10 bg-white flex justify-center items-center rounded-full"
             >
               Live
             </Link>
             <Link
-              href="https://github.com/BarancanS/Baranflix"
+              href="https://github.com/BarancanS/moviessdb"
               target="_blank"
               className="w-20 h-10 bg-orange-400 flex justify-center items-center rounded-full"
             >
