9ae6cad9ea43e5c212b78c1dab3609d25a7e0aab
BarancanS <barancansng@gmail.com>
2023-10-11 16:43:25 +0300

    Update Button.jsx

--- app/components/Button/Button.jsx
+++ app/components/Button/Button.jsx
@@ -4,8 +4,9 @@ const Button = () => {
   return (
     <div>
       <Link
-        target="_blank"
-        href="https://drive.google.com/file/d/1_LHWp8IPYpeDUy4oDGXVrugpEm-4G3Jb/view?usp=sharing"
+        // target="_blank"
+        // href="https://drive.google.com/file/d/1_LHWp8IPYpeDUy4oDGXVrugpEm-4G3Jb/view?usp=sharing"
+        href="https://barancansng.vercel.app/about"
       >
         <button className="transition duration-400 ease-in-out hover:scale-105 mt-10 bg-indigo-900 py-3 px-5 rounded-3xl text-xl max-sm:py-1">
           Detail
